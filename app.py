190ddfcb929c363c48630bd045144d6e24b0d1f4
tongyt <601728814@qq.com>
2023-04-06 19:19:46 +0800

    index app

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 from flask import url_for
 
 app = Flask(__name__)
 
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
-def hello():
-   return '<h1>Hello Gert!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+   return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<name>')
 def user(name):
@@ -14,9 +29,10 @@ def user(name):
 
 @app.route('/test')
 def test_url_for():
-    print(url_for('hello'))
+    print(url_for('index'))
     print(url_for('user', name = 'gert'))
     print(url_for('user', name = 'allen'))
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num=2))
     return 'Test Page'
+
