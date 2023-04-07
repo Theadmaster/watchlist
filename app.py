29234401c0aa62631b8192fd4b8087a2b5ef617d
tongyt <601728814@qq.com>
2023-04-07 10:06:53 +0800

    improve page

--- app.py
+++ app.py
@@ -71,5 +71,9 @@ def test_url_for():
 @app.errorhandler(404)  # 传入要处理的错误代码
 def page_not_found(e):  # 接受异常对象作为参数
     User = db.Model
-    # user = User.query.first()
+    user = User.query.first()
     return render_template('404.html', user=user), 404  # 返回模板和状态码
+
+
+if __name__ == '__main__':
+    app.run(5000)
